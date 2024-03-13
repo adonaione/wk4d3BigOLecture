2a8cb7805b141e4bf0a03c21c32e656072abed0b
adonaione <adonairomero@icloud.com>
2024-03-13 11:08:17 -0600

    in place algo syntax

---
+++ whiteboard/whiteboard.py
@@ -0,0 +1,41 @@
+# You are given a string s representing an attendance record for a student where each character
+#  signifies whether the student was absent, late, or present on that day. The record only 
+# contains the following three characters:
+
+# 'A': Absent.
+# 'L': Late.
+# 'P': Present.
+# The student is eligible for an attendance award if they meet both of the following criteria:
+# The student was absent ('A') for strictly fewer than 2 days total.
+# The student was never late ('L') for 3 or more consecutivedays.
+# Return true if the student is eligible for an attendance award, or false otherwise.
+
+# Example 1:
+# Input: s = "PPALLP"
+# Output: true
+# Explanation: The student has fewer than 2 absences and was never late 3 or more consecutive days.
+# Example 2:
+# Input: s = "PPALLL"
+# Output: false
+# Explanation: The student was late 3 consecutive days in the last 3 days, so is not eligible for the award.
+
+
+#create function called solution
+# loop through the string
+# count the number of vairables
+# if A < 2 
+# if L < 3 
+# get true or false and write explanation 
+
+# def solution(string):
+#     for i in string:
+#         if 'A'
+
+
+def attendance(string):
+    if 'LLL' in string:
+        return False
+    elif string.count('A') >= 2:
+        return False 
+    else:
+        return True
